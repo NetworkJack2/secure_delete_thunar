a826c94cfb6614f03dd818fa1b632b93dd584a2f
GI_Jack <none>
2026-05-20 18:27:57 -0700

    because the --window-icon does not read pixmaps/ with yad, only icons/, we need our own detection logic for full path. This is a kludge as it assumes the /usr/local/share/pixmaps from the Makefile, but the Makefile uses a $(PREFIX), and assumes that PREFIX=/usr will be used for packaging.
    This is the two default use cases on Linux. make install with no opts for
    manual install or make install with PREFIX=/usr for distro packaging.
    
    This makes a big assumption. But as you know, "assume" makes and "ass" out
    of "u" and "me"

--- srm_guified.sh
+++ srm_guified.sh
@@ -21,13 +21,16 @@ DEP_LIST="find shred yad notify-send"
 CONFIRM="N"
 ICON="shred"
 
+[ -f "/usr/local/share/pixmaps/shred.png" ] && ICON_PATH="/usr/local/share/pixmaps/shred.png"
+[ -f "/usr/share/pixmaps/shred.png" ] && ICON_PATH="/usr/share/pixmaps/shred.png"
+
 exit_with_error(){
   local -i win_length=300
   local -i win_height=12
   echo 1>&2 "srm_guified.sh: ERROR: ${2}"
   notify-send --icon ${ICON} "Secure Delete: ERROR!" "${2} (${1})"
   #Xdialog --icon ${ICON} --title "Secure Delete" --msgbox "${2} (${1})" ${win_height} ${win_length}
-  yad --error --window-icon ${ICON} --image="dialog-error" --title "Secure Delete" --text "${2} (${1})" --button="yad-ok" --height=${win_height} --width=${win_length}
+  yad --error --window-icon="${ICON_PATH}" --image="dialog-error" --title "Secure Delete" --text "${2} (${1})" --button="yad-ok" --height=${win_height} --width=${win_length}
   exit ${1}
 }
 
@@ -68,7 +71,7 @@ confirm_delete() {
      ;;
    *)
     #Xdialog --icon ${ICON} --title "Secure Delete" --yesno "Really Wipe ${NUM_FILES} File(s)?" ${win_height} ${win_length}
-    yad --window-icon=${ICON} --image=${ICON} --title "Secure Delete" --text "Really Wipe ${NUM_FILES} File(s)?" --button="yad-yes:0" --button="yad-no:1" --height=${win_height} --width=${win_length}
+    yad --window-icon="${ICON_PATH}" --image=${ICON} --title="Secure Delete" --text="Really Wipe ${NUM_FILES} File(s)?" --button="yad-yes:0" --button="yad-no:1" --height=${win_height} --width=${win_length}
     exit_code=${?}
     ;;
   esac
@@ -130,7 +133,7 @@ delete_files() {
       sleep ${fin_wait}
     ) |
     #Xdialog --icon ${ICON} --title "Secure Delete" --gauge "Wiping ${NUM_FILES} file(s)" ${win_height} ${win_length}
-    yad --progress --window-icon=${ICON} --image=${ICON} --title "Secure Delete" --text "Wiping ${NUM_FILES} file(s)" --button="yad-cancel:255" --auto-close --height=${win_height} --width=${win_length}
+    yad --progress --window-icon="${ICON_PATH}" --image=${ICON} --title "Secure Delete" --text "Wiping ${NUM_FILES} file(s)" --button="yad-cancel:255" --auto-close --height=${win_height} --width=${win_length}
     [ $? -eq 255 ] && exit_with_error 4 "Secure Wipe Aborted"
     ;;
   esac
